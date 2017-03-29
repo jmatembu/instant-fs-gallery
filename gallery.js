0c238026014ef3732c7c2435429c65b693c723ff
Joseph M. Matembu <jmatembu@gmail.com>
2017-03-29 16:48:20 +0300

    Improve gallery

--- gallery.js
+++ gallery.js
@@ -12,6 +12,7 @@
         prevBtn: '.su_icon--prev'
       }
     },
+    slideIndicatorPositions: {},
 
     /**
      * Get the fullscreen carousel element
@@ -52,36 +53,75 @@
 
     openCarousel: function () {
 
-      this.loadSlideIndicators('hr');
+      this.loadSlideIndicators('hr');  
+
       this.getCarouselEl().classList.add('su_fs-carousel--active');
 
     },
 
-    loadSlideIndicators: function (el) {
+    loadSlideIndicators: function () {
 
       var slideWrapper = this.getElement('.su_fs-slides'),
-          num = this.getSlides().length * 2,
-          hr, val, bottom, i;
+          num = this.getSlides().length,
+          fragment = document.createDocumentFragment(),
+          hr, val, bottom, top, i, position;
 
       if ( slideWrapper ) {
 
-        for ( i = 0; i < num; i = i + 2 ) {
+        for ( i = 0; i < num; i++ ) {
 
-          hr = document.createElement(el);
-          val = 6 - i + 2 + 'px';
-          bottom = 'bottom: calc(100% - ' + val;
-          hr.style.cssText = bottom;
+          position = {}; // reset object
+          hr = document.createElement('hr');
+          val = 8 - i - i + 'px';
+          top = 'bottom: calc(100% - ' + val + ')';
+          val = 47 - i - i;
+          bottom = 'bottom: ' + val + 'px';
+          position.top = top;
+          position.bottom = bottom;
 
-          slideWrapper.appendChild(hr);
+          this.slideIndicatorPositions[i] = position;
+          hr.style.cssText = top;
+
+          fragment.appendChild(hr);
 
         }
+
+        slideWrapper.appendChild(fragment);
+
       }
+
       
+      
+    },
+
+    positionSlideIndicators: function (index, num) {
+
+      var hr, bottom, val, positions = [];
+
+      for ( index = 0; index < num; index = index + 2 ) {
+
+        hr = document.createElement(el);
+        val = 6 - index + 2 + 'px';
+        bottom = 'bottom: calc(100% - ' + val + ')';
+        positions.push(bottom);
+
+      }
+
+      return positions;
+    },
+
+    positionActiveSlideIndicator: function () {
+
+      var rulers = this.getElements('.su_fs-slides > hr'),
+          index = this.currentSlidePosition();
+      
+      if ( rulers ) {
+        rulers[index].style.cssText = this.slideIndicatorPositions[index].bottom;
+      }
 
     },
 
-
-    currentSlide: function () {
+    currentSlidePosition: function () {
       var slides = this.getSlides(),
           currentIndex;
           
@@ -96,43 +136,53 @@
 
     nextSlide: function () {
       var slides = this.getSlides(),
-          index = this.currentSlide(),
-          prevBtn = this.getElement(this.elements.buttons.prevBtn);
-          //hr = this.getElement(this.elements.slide + ' hr');
+          index = this.currentSlidePosition(),
+          prevBtn = this.getElement(this.elements.buttons.prevBtn),
+          rulers = this.getElements('.su_fs-slides > hr');
 
-      if ( prevBtn.classList.contains('su_hidden') ) {
-        prevBtn.classList.remove('su_hidden');
-      }
+      // if ( prevBtn.classList.contains('su_hidden') ) {
+      //   prevBtn.classList.remove('su_hidden');
+      // }
 
       if ( index + 1 < slides.length ) {
         slides[index].classList.remove('su_fs-slide--active');
         slides[index + 1].classList.add('su_fs-slide--active');
-        //slides[index + 1].querySelector('hr').style.cssText = 'bottom: 37px;';
-        if ( index + 1 === slides.length - 1 ) {
-          this.getElement(this.elements.buttons.nextBtn).classList.add('su_hidden');
-        }
+        rulers[index + 1].style.cssText = this.slideIndicatorPositions[index + 1].bottom;
       }
+      
+      
+      //   if ( index + 1 === slides.length - 1 ) {
+      //     this.getElement(this.elements.buttons.nextBtn).classList.add('su_hidden');
+      //   }
+
+        
+      // }
     },
 
     prevSlide: function () {
       var slides = this.getSlides(),
-          index = this.currentSlide(),
+          index = this.currentSlidePosition(),
           slideNum = slides.length,
-          nextBtn = this.getElement(this.elements.buttons.nextBtn);
-          //hr = this.getElement(this.elements.slide + ' hr');
+          nextBtn = this.getElement(this.elements.buttons.nextBtn),
+          rulers = this.getElements('.su_fs-slides > hr');
 
-      if ( nextBtn.classList.contains('su_hidden') ) {
-        nextBtn.classList.remove('su_hidden');
-      }
+
+      // if ( nextBtn.classList.contains('su_hidden') ) {
+      //   nextBtn.classList.remove('su_hidden');
+      // }
 
       if ( index - 1 >= 0 ) {
         slides[index].classList.remove('su_fs-slide--active');
         slides[index - 1].classList.add('su_fs-slide--active');
-        //slides[index - 1].querySelector('hr').style.cssText = 'bottom: 37px;';
-        if ( index - 1 === 0 ) {
-          this.getElement(this.elements.buttons.prevBtn).classList.add('su_hidden');
-        }
+        //console.log(rulers[index]);
+        rulers[index - 1].style.cssText = this.slideIndicatorPositions[index - 1].top;
       }
+      
+      
+      //   if ( index - 1 === 0 ) {
+      //     this.getElement(this.elements.buttons.prevBtn).classList.add('su_hidden');
+      //   }
+      // }
     }
 
   },
@@ -143,7 +193,9 @@
     element.addEventListener('mouseover', function (e) {
       
       fsCarousel.openCarousel();
+      
       this.classList.add('su_fs-slide--active');
+      fsCarousel.positionActiveSlideIndicator();
 
     }, false);
   });
@@ -161,8 +213,9 @@
   });
 
   fsCarousel.getElement(fsCarousel.elements.buttons.closeBtn).addEventListener('click', function (event) {
-    var activeSlie = fsCarousel.getElement('.su_fs-slide--active');
+    
     fsCarousel.closeCarousel();
+    var activeSlie = fsCarousel.getElement('.su_fs-slide--active');
 
     activeSlie && activeSlie.classList.remove('su_fs-slide--active');
   }, false);
